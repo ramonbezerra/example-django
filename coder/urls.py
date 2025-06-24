ecb1823f4a1287c7ead6cd2601b8525d6ea650aa
ramonbezerra <ramon.bezerra@outlook.com>
2025-06-24 11:14:20 -0300

    feat: create list and detail extends template

--- coder/urls.py
+++ coder/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import about
+from .views import about, index, detail
 
 appname = 'coder'
 
 urls = [
     path('about/', about, name='about'),
+    path('', index, name='index'),
+    path('detail/<int:student_id>/', detail, name='detail'),
 ]
 
 coder_patterns = (urls, appname)
--- coder/views.py
+++ coder/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 # Create your views here.
 def about(request):
     return HttpResponse("Hello, this is the about page of the coder app.")
+
+def index(request):
+    context = {'students': Student.objects.all()}
+    return render(request, 'coder/index.html', context)
+
+def detail(request, student_id):
+    context = {'student': Student.objects.get(id=student_id)}
+    return render(request, 'coder/detail.html', context)
